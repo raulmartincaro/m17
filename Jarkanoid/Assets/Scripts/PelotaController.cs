777f2ee654d6ebf12b7e8ae5de25c1d323c3b58f
raulmartincaro <raul.martincaro@gmail.com>
2023-09-18 21:30:53 +0200

    pelota no bota

--- Jarkanoid/Assets/Scripts/PelotaController.cs
+++ Jarkanoid/Assets/Scripts/PelotaController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PelotaController : MonoBehaviour
 {
+    public Rigidbody2D m_rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidbody = GetComponent<Rigidbody2D>();
+
+        m_rigidbody.velocity = new Vector3(1,1,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       // m_rigidbody.velocity = new Vector3(1, 1, 0);
+
     }
 }
