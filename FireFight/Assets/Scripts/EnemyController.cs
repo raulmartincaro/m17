f02bc342b59bb9f9d0e18f0bb0deb7370a4978bf
raulmartincaro <raul.martincaro@gmail.com>
2023-10-17 18:39:39 +0200

    2.2
    cosas finales

--- FireFight/Assets/Scripts/EnemyController.cs
+++ FireFight/Assets/Scripts/EnemyController.cs
@@ -15,4 +15,13 @@ public class EnemyController : MonoBehaviour
     {
         
     }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="Hitbox") 
+        { 
+
+        }
+    }
 }
